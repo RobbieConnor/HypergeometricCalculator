c6c911282c30a7203da77cc40d2a8770ec8afa1c
Robbie Connor <rconnor07@qub.ac.uk>
2018-11-10 14:00:21 +0000

    Updated exception javadoc

--- src/com/robbieconnor/HypergeometricCalculator.java
+++ src/com/robbieconnor/HypergeometricCalculator.java
@@ -28,8 +28,27 @@ import java.math.BigDecimal;
 
 public class HypergeometricCalculator {
 
-    //This code is the hypergeometric calculator's calculation.
-    public static double calculate(int totalNumberOfObjects, int numberOfSuccesses, int sampleSize, int successesNeeded) {
+    /**
+     * Calculate the hypergeometric probability based on the parameters passed in
+     * @param totalNumberOfObjects Total number of objects in the population
+     * @param numberOfSuccessesPossible Number of successes possible in the population
+     * @param sampleSize Size of the sample
+     * @param successesNeeded Number of successes needed in the sample
+     * @return The hypergeometric probability of attaining that amount of successes in a sample
+     * @throws ArithmeticException If number of successes needed is greater than total number of successes in population
+     * @throws ArithmeticException If number of successes possible is greater than the total number of objects
+     * @throws IllegalArgumentException If an input parameter is less than or equal to 0
+     */
+    public static double calculate(int totalNumberOfObjects, int numberOfSuccessesPossible, int sampleSize, int successesNeeded) throws ArithmeticException, IllegalArgumentException {
+
+        if (totalNumberOfObjects <= 0 || numberOfSuccessesPossible <= 0 || sampleSize <= 0 || successesNeeded <= 0) {
+            throw new IllegalArgumentException("Negative number in calculation");
+        } else if(successesNeeded > numberOfSuccessesPossible) {
+            throw new ArithmeticException("Number of successes needed must be less than or equal to the number of successes in the population");
+        } else if (totalNumberOfObjects < numberOfSuccessesPossible) {
+            throw new ArithmeticException("Number of successes possible must be greater than or equal to total number of objects");
+        }
+
 
         //Processed value to be returned at the end of the calculation.
         double probability;
@@ -41,10 +60,10 @@ public class HypergeometricCalculator {
         BigDecimal NkCnx;
         BigDecimal NCn;
 
-        kCx = nCr(numberOfSuccesses, successesNeeded);
+        kCx = nCr(numberOfSuccessesPossible, successesNeeded);
         int kCxConverted = Integer.parseInt(kCx.toString());
 
-        NkCnx = nCr((totalNumberOfObjects - numberOfSuccesses), (sampleSize - successesNeeded));
+        NkCnx = nCr((totalNumberOfObjects - numberOfSuccessesPossible), (sampleSize - successesNeeded));
         int NkCnxConverted = Integer.parseInt(NkCnx.toString());
 
         NCn = nCr(totalNumberOfObjects, sampleSize);
